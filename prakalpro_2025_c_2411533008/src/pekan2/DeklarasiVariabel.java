83e3c386f893813c2be07f8393bb6222d8b3ecbb
Arya pratama hendri <aryaselalu2506@gmail.com>
2025-09-16 11:18:10 +0700

    Add files via upload

---
+++ prakalpro_2025_c_2411533008/src/pekan2/DeklarasiVariabel.java
@@ -0,0 +1,23 @@
+package pekan2;
+
+public class DeklarasiVariabel {
+	/* program java
+	 * latihan
+	 * tentang pendeklarasikan variabel
+	 */
+	static int umur=25; 
+	public static void main(String[] args) {
+		int kode;
+		boolean isDibawahUmur; 
+		kode = 1234;
+		double gaji;
+		gaji = 5500000.23;
+		isDibawahUmur = true;
+		System.out.println("Status: "+isDibawahUmur);
+		System.out.println("kode:"+kode);
+		System.out.println("Umur:"+umur);
+		System.out.println("Gaji:"+gaji);
+	
+	}
+
+}
---
+++ prakalpro_2025_c_2411533008/src/pekan2/KelilingLingkaran.java
@@ -0,0 +1,12 @@
+package pekan2;
+
+public class KelilingLingkaran {
+
+	public static void main(String[] args) {
+		final double PI = 3.14; /* Denifisi Konstanta */
+		double radius = 30;     /* Deklarasi variabel */
+		System.out.println("Keliling = " + 2 * PI * radius);
+		
+	}
+
+}
